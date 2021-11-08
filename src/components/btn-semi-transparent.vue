f2b975d91f83937ce3d2357606c99a740a8ab7f6
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-11-08 19:13:32 +0200

    1.0.5

--- src/components/btn-semi-transparent.vue
+++ src/components/btn-semi-transparent.vue
@@ -1,22 +1,32 @@
 <template>
   <button class="btn-wrp">
-    <a href="#">Узнай детали</a>
+    <a href="#">{{ details }}</a>
   </button>  
 </template>
 
 <script>
 export default {
   name: 'btnSemiTransparent',
-//   props: {
-//     msg: String
-//   }
+  data() {
+      return {
+          details: "Узнай детали"
+      }
+    
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import "../scss/_main_variable_gv.scss";
+@import "../scss/_styleAdaptive.scss";
 .btn-wrp {
     @include btnCardTransparent;
+    @include mobileHeight {
+      & {
+        max-width: 510px;
+        width: 100%;
+      }
+    }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import store from './store';
 import starRating from './starRating.vue';
-import buttomSemiTransparent from './btnSemiTransparent.vue';
+import btnTrans from './components/btn-semi-transparent.vue';
 
 createApp(App).use(store).mount('#app');
 createApp(starRating).use(starRating).mount('#starRating');
-createApp(buttomSemiTransparent).use(buttomSemiTransparent).mount('#buttomSemiTransparent');
 
 // createApp(StarRating).use(StarRating).mount('#StarRating')
+
+const vueBtns = document.querySelectorAll('.vue-buttomSemiTransparent');
+for (const vueBtn of vueBtns) {
+    createApp(btnTrans).use(store).mount(vueBtn);
+}
