789fec9369b6fb57b49efad00d51954174991f3d
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-12-06 15:17:21 +0200

    1010.0

--- src/components/ratingReviews.vue
+++ src/components/ratingReviews.vue
@@ -1,24 +1,69 @@
 <template>
-  <div class="overall-rating-card-prod">
-    <p class="overall-rating-card-prod__title">Общий рейтинг</p>
-    <div class="overall-rating-card-prod__wrp">
-      <div>
-        <p class="overall-rating-card-prod__num">5,0</p>
-        <starReviews class="overall-rating-card-prod__star"></starReviews>
-        <p class="overall-rating-card-prod__text">{{ numberOfReviews }} отзывов</p>
-      </div>
-      <div class="rating-scale">
+  <div class="reviews-page-top-card">
+    <div class="overall-rating-card-prod">
+      <p class="overall-rating-card-prod__title">Общий рейтинг</p>
+      <div class="overall-rating-card-prod__wrp">
+        <div>
+          <p class="overall-rating-card-prod__num">5,0</p>
+          <starReviews class="overall-rating-card-prod__star"></starReviews>
+          <p class="overall-rating-card-prod__text">{{ numberStarsFive }} отзывов</p>
+        </div>
+        <div class="rating-scale">
           <div class="rating-scale__wrp">
               <p class="rating-scale__num">5</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
               <div class="rating-scale__pos-wrp">
                 <div v-bind:style="{ width: `${averageFive * 10}%` }" class="rating-scale__act"></div>
                 <div class="rating-scale__des"></div>
               </div>
-                <p class="rating-scale__num-style">{{ numberStars }}</p>
+                <p class="rating-scale__num-style">{{ numberStarsFive }}</p>
           </div>
+          <div class="rating-scale__wrp">
+              <p class="rating-scale__num">4</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
+              <div class="rating-scale__pos-wrp">
+                <div v-bind:style="{ width: `${averageFour * 10}%` }" class="rating-scale__act"></div>
+                <div class="rating-scale__des"></div>
+              </div>
+                <p class="rating-scale__num-style">{{ numberStarsFour }}</p>
+          </div>
+          <div class="rating-scale__wrp">
+              <p class="rating-scale__num">3</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
+              <div class="rating-scale__pos-wrp">
+                <div v-bind:style="{ width: `${averageThree * 10}%` }" class="rating-scale__act"></div>
+                <div class="rating-scale__des"></div>
+              </div>
+                <p class="rating-scale__num-style">{{ numberStarsThree }}</p>
+          </div>
+          <div class="rating-scale__wrp">
+              <p class="rating-scale__num">2</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
+              <div class="rating-scale__pos-wrp">
+                <div v-bind:style="{ width: `${averageTwo * 10}%` }" class="rating-scale__act"></div>
+                <div class="rating-scale__des"></div>
+              </div>
+                <p class="rating-scale__num-style">{{ numberStarsTwo }}</p>
+          </div>
+          <div class="rating-scale__wrp">
+              <p class="rating-scale__num">2</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
+              <div class="rating-scale__pos-wrp">
+                <div v-bind:style="{ width: `${averageTwo * 10}%` }" class="rating-scale__act"></div>
+                <div class="rating-scale__des"></div>
+              </div>
+                <p class="rating-scale__num-style">{{ numberStarsTwo }}</p>
+          </div>
+          <div class="rating-scale__wrp">
+              <p class="rating-scale__num">1</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
+              <div class="rating-scale__pos-wrp">
+                <div v-bind:style="{ width: `${numberStarsOne * 10}%` }" class="rating-scale__act"></div>
+                <div class="rating-scale__des"></div>
+              </div>
+              <p class="rating-scale__num-style">{{ averageOne }}</p>
+          </div>
+        </div>
       </div>
     </div>
-  </div> 
+    <div class="reviews-slider-card-prod">
+      <h4 class="reviews-slider-card-prod__title">Фото и видео покупателей</h4>
+    </div> 
+  </div>  
 </template>
 
 <script>
@@ -31,11 +76,19 @@ export default {
     starReviews
   },
   data () {
-      return {
-        numberStars: 15,
-        averageFive: 2,
-        numberOfReviews: 2
-      }
+    return {
+      numberStarsFive: 15,
+      numberStarsFour: 1,
+      numberStarsThree: 3,
+      numberStarsTwo: 5,
+      numberStarsOne: 1,
+      averageFive: 2,
+      averageFour: 5,
+      averageThree: 4,
+      averageTwo: 2,
+      averageOne: 0,
+      numberOfReviews: 2,
+    }
   }
 }
 </script>
