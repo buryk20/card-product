3bfc54cca938a8bcdc6c37a2a4c14138958d3602
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-12-07 07:15:01 +0200

    slider_rev

--- src/components/ratingReviews.vue
+++ src/components/ratingReviews.vue
@@ -42,14 +42,6 @@
                 <p class="rating-scale__num-style">{{ numberStarsTwo }}</p>
           </div>
           <div class="rating-scale__wrp">
-              <p class="rating-scale__num">2</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
-              <div class="rating-scale__pos-wrp">
-                <div v-bind:style="{ width: `${averageTwo * 10}%` }" class="rating-scale__act"></div>
-                <div class="rating-scale__des"></div>
-              </div>
-                <p class="rating-scale__num-style">{{ numberStarsTwo }}</p>
-          </div>
-          <div class="rating-scale__wrp">
               <p class="rating-scale__num">1</p><img class="rating-scale__star-icon" src="/icon/star-card-product.svg" alt="stars">
               <div class="rating-scale__pos-wrp">
                 <div v-bind:style="{ width: `${numberStarsOne * 10}%` }" class="rating-scale__act"></div>
@@ -62,18 +54,21 @@
     </div>
     <div class="reviews-slider-card-prod">
       <h4 class="reviews-slider-card-prod__title">Фото и видео покупателей</h4>
+      <sliderRev :slider_data="sliderItems"></sliderRev>
     </div> 
   </div>  
 </template>
 
 <script>
 
-import starReviews from './starReviews.vue'
+import starReviews from './starReviews.vue';
+import sliderRev from './slider-reviews.vue'
 
 export default {
   name: 'ratingReview',
   components: {
-    starReviews
+    starReviews,
+    sliderRev
   },
   data () {
     return {
@@ -88,6 +83,15 @@ export default {
       averageTwo: 2,
       averageOne: 0,
       numberOfReviews: 2,
+      sliderItems: [
+            { id: 1, name: 'img1', img: '1.png'},
+            { id: 2, name: 'img2', img: '6626_2.png'},
+            { id: 3, name: 'img2', img: '6626_3.png'},
+            { id: 4, name: 'img2', img: '6626_4.png'},
+            { id: 5, name: 'img2', img: '6626_5.png'},
+            { id: 6, name: 'img2', img: '6626_4.png'},
+            { id: 7, name: 'img2', img: '6626_3.png'}
+          ]
     }
   }
 }
